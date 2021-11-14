ee76f875a98cc1cbbde470125336df9772c9bd7a
Elvis Borgström <Elvis.borgstrom@gmail.com>
2021-11-14 16:39:02 +0100

    uppdatering

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
-let deck = {}; // Skapar en variabel för att spara vårat deck i.
- async function getDeck() {
+let deck={}; //skapar en variabel för att spara vårat deck i
 
-   // En asynchron funktion som vi anropar från root för att hämta vårat deck så fort vår kod laddas och exekveras
+const smallPokeDex = document.getElementById("smallPokeDex");
+const lowerbutton = document.getElementById("lower");
+const higherbutton = document.getElementById("higher");
+
+
+async function getDeck(){ 
+    const res = await fetch(
+        "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+    );
+    const data = await res.json(); // Löser ut body från vårat response och gör det till en 
+
+    console.log(data);
+    deck = data; // assignar data till vårat deck så att vi kan använda variabeln senare
+    console.log(deck);
+}
+
+getDeck(); // Anropar funktionen direkt
+
+const drawCardButt = document.getElementById("drawCard"); // Hämtar våran knapp vi behöver för att dra kort
+
+
+drawCardButt.addEventListener("click", async () => {
+  // Funktionen anropar api:et och får en array av kort plus lite response data
   const res = await fetch(
-     "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1" // Kolla dokumentationen för api:et hur ni kan skicka in variabler, men här väljer vi att vi bara ska ett deck
-   );
-   const data = await res.json(); // Vi löser ut body från vårat response och gör om det till ett javascriptobjekt
+    https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1
+  );
+  const data = await res.json();
 
-   console.log(data);
-   deck = data; // Vi assignar data till vårat deck så vi kan använda variabeln senare
-   console.log(deck);
- }
+  const displayCard = data.cards[0];
 
- getDeck(); // Vi anropar funktionen direkt
+  console.log(data); // Loggar det första kortet i resultatet då jag bara ville dra 1 kort.
 
- const drawCardButt = document.getElementById("drawCard"); // Hämtar våran knapp vi behöver för att dra kort
- drawCardButt.addEventListener("click", async () => {
-   
-    // Funktionen anropar api:et och får en array av kort plus lite response data
-   const res = await fetch(
-     `https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`
-   );
-   const data = await res.json();
-   console.log(data.cards[0]); // Loggar det första kortet i resultatet då jag bara ville dra 1 kort.
- });
+  smallPokeDex.children[0].innerText = displayCard.code;
+  smallPokeDex.children[1].setAttribute("src", displayCard.image);
 
- 
+});
