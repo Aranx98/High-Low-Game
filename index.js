9ad090fa2d831da25161f969b1bf4381a72dab3c
Elvis Borgström <Elvis.borgstrom@gmail.com>
2021-11-16 17:18:25 +0100

    update js

--- index.js
+++ index.js
@@ -1,70 +1,84 @@
 let deck={}; //skapar en variabel för att spara vårat deck i
 let currentCard = 0;
 let oldCard = 0;
+let ArraywithCards = [];
 
 const card = document.getElementById("card");
 const lowerButton = document.getElementById("lower");
 const higherButton = document.getElementById("higher");
 const drawCardButton = document.getElementById("drawCard");
 
+
+
 async function getDeck(){ 
     const res = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1" );
     const data = await res.json(); // Löser ut body från vårat response och gör det till en 
-    console.log(data);
+    
     deck = data; // assignar data till vårat deck så att vi kan använda variabeln senare
-    console.log(deck);
+    
 }
 
 getDeck(); // Anropar funktionen direkt
 
-async function drawnewCard() {
+async function drawFirstCard() {
   const res = await fetch(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`);
   const data = await res.json();
-  //console.log(data.cards[0]); // Logging the first card
+  
   image.setAttribute("src", data.cards[0].image);
   currentCard = data.cards[0].value;
-  console.log(data.cards[0]);
   currentCard = convertRoyals(currentCard);
-  }
+
+  return oldCard;
+
+}
+
+async function drawnewCard() {
+  const res =await fetch(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`);
+  const data = await res.json();
+
+  image.setAttribute("src", data.cards[0].image);
+  currentCard =convertRoyals(data.cards[0].value);
+  currentCard = data.cards[0].value;
+
+  return currentCard;
+
+}
 
 drawCardButton.addEventListener("click", async() => {
   const res = await fetch(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`);
   const data = await res.json();
-  //console.log(data.cards[0]); // Logging the first card
-  image.setAttribute("src", data.cards[0].image);
-  oldCard = data.cards[0].value;
-  oldCard = convertRoyals(oldCard);
+  const firstCard = await drawFirstCard();
+
   console.log(data.cards[0]);
+  ArraywithCards.push(firstCard);
+
 });
 
 
 lowerButton.addEventListener("click", async() => {
-  await drawnewCard();
-  answer("false");
-  console.log("I choose the button lower");
+  await lower();
+ 
 });
 
 higherButton.addEventListener("click", async() => {
-  await drawnewCard();
-  answer("true");
-  console.log("I choose the button high");
+  await higher();
+  
 });
 
-async function answer(guess) {
-  let correctAnswer = '';
-  let firstCard = oldCard;
-  let secondCard = currentCard;
+async function lower() {
+  const currentCard = await drawnewCard();
+  ArraywithCards.push(currentCard); 
 
-  if(secondCard > firstCard) {
-    correctAnswer ="higher";
-  } else {
-    correctAnswer = "lower";
+  if(ArraywithCards[0] < ArraywithCards[1]) { //Jämför det första kortet med det andra.
+    console.log("Du svarade rätt, kortet var lägre.");
+
+  } else { 
+    console.log("Du svarade fel, kortet var högre.")
   }
-  if(guess == correctAnswer) {
-    console.log("It was right");
-  } else {
-    console.log("It was wrong");
-  }
+   ArraywithCards.shift();
+ 
+
+  
 
 }
 
